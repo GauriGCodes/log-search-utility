2fd65453569198049035c14e91156a9b0c2553a1
Gauri Gupta <gg18753n@pace.edu>
2024-02-23 22:40:42 -0600

    Spring Security LDAP added

--- src/main/java/com/company/log/search/utility/service/WebSecurityConfig.java
+++ src/main/java/com/company/log/search/utility/service/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package com.company.log.search.utility.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-
+@Configuration
 public class WebSecurityConfig {
-/*
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests((authorize) -> authorize.anyRequest().fullyAuthenticated()).formLogin((Customizer.withDefaults()));
@@ -23,5 +31,4 @@ public class WebSecurityConfig {
                 .passwordEncoder(new BCryptPasswordEncoder())
                 .passwordAttribute("userPassword");
     }
-*/
 }
